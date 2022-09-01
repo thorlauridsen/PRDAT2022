556c12da6352439a0ed92ca2c30d1a12212dabb8
Philip Kristian Møller Flyvholm <phif@itu.dk>
2022-09-01 11:31:30 +0200

    1.2

--- Lectures/Lec01/Intro/Intro2.fs
+++ Lectures/Lec01/Intro/Intro2.fs
@@ -23,7 +23,8 @@ let cvalue = lookup env "c";;
 type expr = 
   | CstI of int
   | Var of string
-  | Prim of string * expr * expr;;
+  | Prim of string * expr * expr
+  | If of expr * expr * expr;;
 
 let e1 = CstI 17;;
 
@@ -33,7 +34,7 @@ let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 
 
 (* Evaluation within an environment *)
-(* 1.1 *)
+(* 1.1.1 *)
 let rec eval e (env : (string * int) list) : int =
     match e with
     | CstI i            -> i
@@ -73,3 +74,54 @@ printfn "eval (Prim(\"==\", CstI 5, CstI 10)) = %A" (example3)
 let example4 = eval (Prim("==", CstI 10, CstI 10)) env //Returns 1
 printfn "eval (Prim(\"==\", CstI 10, CstI 10)) = %A" (example4)
 
+(* 1.1.2, 1.1.3 1.1.4*)
+let rec eval2 e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x 
+    | Prim(ops, e1,e2) ->
+        let i1 = eval2 e1 env
+        let i2 = eval2 e2 env
+        match ops with
+        | "+" -> i1+i2
+        | "*" -> i1*i2
+        | "-" -> i1-i2
+        | "max" ->  if i1 > i2 then i1 else i2
+        | "min" ->  if i1 < i2 then i1 else i2
+        | "==" ->  if i1 = i2 then 1 else 0
+        | _ -> failwith "unknown primitive"
+    | If (e1, e2, e3) -> if eval2 e1 env <> 0 then eval2 e2 env else eval2 e3 env
+
+(*1.2.1*)
+
+type aexpr =
+  | CstI of int
+  | Var of string
+  | Add of aexpr * aexpr
+  | Mul of aexpr * aexpr
+  | Sub of aexpr * aexpr
+
+
+(* 1.2.2 *)
+(* v−(w+z) *)
+let aex1 = Sub(Var "v", Add (Var "w", Var "z"))
+
+(* 2*(v−(w+z)) *)
+let aex2 = Mul(CstI 2, Sub(Var "v", Add (Var "w", Var "z")))
+
+(* x + y + z + v *)
+let aex3 = Add(Var "x", Add (Var "y", Add (Var "z", Var "v")))
+
+(* 1.2.3 *)
+let rec fmt =
+    function
+    | CstI i -> string i
+    | Var s -> s
+    | Add (a1, a2) ->
+        "(" + fmt a1 + "+" + fmt a2 + ")"
+    | Mul (a1, a2) ->
+        "(" + fmt a1 + "*" + fmt a2 + ")"
+    | Sub (a1, a2) ->
+        "(" + fmt a1 + "-" + fmt a2 + ")"
+
+printfn "(x - 34) = %A" (fmt (Sub(Var "x", CstI 34)))
