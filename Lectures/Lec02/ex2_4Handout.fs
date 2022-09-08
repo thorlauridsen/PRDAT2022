366f32e4a8525cc694dd3844466ec72231bc9f0e
daso <daso@itu.dk>
2022-09-08 10:14:42 +0200

    @Exercise 2.4.1

--- Lectures/Lec02/ex2_4Handout.fs
+++ Lectures/Lec02/ex2_4Handout.fs
@@ -2,19 +2,20 @@
 
 (* Ex 2.4 - assemble to integers *)
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
-let sinstrToInt = function
+let sinstrToInt : list = function
   | SCstI i -> [0;i]
-  | SVar i  -> failwith "Not implemented"
-  | SAdd    -> failwith "Not implemented"
-  | SSub    -> failwith "Not implemented"
-  | SMul    -> failwith "Not implemented"
-  | SPop    -> failwith "Not implemented"
-  | SSwap   -> failwith "Not implemented"
+  | SVar i  -> [1;i]
+  | SAdd    -> [2]
+  | SSub    -> [3]
+  | SMul    -> [4]
+  | SPop    -> [5]
+  | SSwap   -> [6]
 
-let assemble instrs = failwith "Not implemented"
+let assemble (instrs : sinstr list) : int list = List.fold (fun acc item -> (sinstrToInt item)@acc) [] instrs
 
 (* Output the integers in list inss to the text file called fname: *)
 
 let intsToFile (inss : int list) (fname : string) = 
     let text = String.concat " " (List.map string inss)
     System.IO.File.WriteAllText(fname, text);;
+
