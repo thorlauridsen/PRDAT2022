d7984f2cd2387dc37b42d518717b85a6bf301490
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2022-09-08 10:23:14 +0200

    Fix

--- Lectures/Lec02/ex2_4Handout.fs
+++ Lectures/Lec02/ex2_4Handout.fs
@@ -2,14 +2,14 @@
 
 (* Ex 2.4 - assemble to integers *)
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
-let sinstrToInt : list = function
+let sinstrToInt = function
   | SCstI i -> [0;i]
   | SVar i  -> [1;i]
   | SAdd    -> [2]
   | SSub    -> [3]
   | SMul    -> [4]
   | SPop    -> [5]
-  | SSwap   -> [6]
+  | SSwap   -> [6] : int list 
 
 let assemble (instrs : sinstr list) : int list = List.fold (fun acc item -> (sinstrToInt item)@acc) [] instrs
 
@@ -19,3 +19,5 @@ let intsToFile (inss : int list) (fname : string) =
     let text = String.concat " " (List.map string inss)
     System.IO.File.WriteAllText(fname, text);;
 
+let intList = [0; 17; 1; 0; 1; 1; 2; 6; 5]
+let createFile = intsToFile intList "is2.txt"
