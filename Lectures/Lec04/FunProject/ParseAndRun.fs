c4c33cae8468a6bf1b3ce656b37efe353da8bb8c
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2022-09-22 10:57:53 +0200

    Exercise 4.2

--- Lectures/Lec04/FunProject/ParseAndRun.fs
+++ Lectures/Lec04/FunProject/ParseAndRun.fs
@@ -15,3 +15,8 @@ let e3 = run (fromString "let f x = x + 7 in f 2 end");;
 let ex1 = run (fromString "5+7");;
 let ex2 = run (fromString "let y = 7 in y + 2 end");;
 let ex3 = run (fromString "let f x = x + 7 in f 2 end");;
+
+
+run (fromString "let sum n = if n < 0 then n + sum(n-1) else 0 in sum 1000 end");;
+run (fromString "let sum n = if n = 0 then 0 else n + sum(n-1) in sum 1000 end");;
+run (fromString "let pow8 n = if n = 0 then 1 else n * pow(n-1) in pow 8 ");;
