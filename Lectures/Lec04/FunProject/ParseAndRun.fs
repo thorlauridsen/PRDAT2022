97671d89bcb57cfa8be3018bf447089236692536
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2022-09-22 10:27:35 +0200

    Add exercise 4.1

--- Lectures/Lec04/FunProject/ParseAndRun.fs
+++ Lectures/Lec04/FunProject/ParseAndRun.fs
@@ -7,3 +7,11 @@ let fromString = Parse.fromString;;
 let eval = Fun.eval;;
 
 let run e = eval e [];;
+
+let e1 = run (fromString "5+7");;
+let e2 = run (fromString "let y = 7 in y + 2 end");;
+let e3 = run (fromString "let f x = x + 7 in f 2 end");;
+
+let ex1 = run (fromString "5+7");;
+let ex2 = run (fromString "let y = 7 in y + 2 end");;
+let ex3 = run (fromString "let f x = x + 7 in f 2 end");;
