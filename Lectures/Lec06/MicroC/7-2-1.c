8baa42b6613968eb845d99dbb3f7ef2136b1cda1
Philip Kristian Møller Flyvholm <phif@itu.dk>
2022-10-13 12:08:44 +0200

    7.2.1

--- Lectures/Lec06/MicroC/7-2-1.c
+++ Lectures/Lec06/MicroC/7-2-1.c
@@ -4,14 +4,25 @@ int main() {
     arr[1] = 13;
     arr[2] = 9;
     arr[3] = 8;
-    int *sump;
+    int sump;
+    sump = 0;
     arrsum(4, arr, &sump);
-    print *sump; 
+    print sump;
 }
 
+//Using recursion
 void arrsum(int n, int arr[], int *sump) {
     if(n > 0){
-        *sump = arr[n] + *sump;
-        arrsum(n-1, arr, &sump);
+        *sump = (*sump + arr[n-1]);
+        arrsum(n-1, arr, sump);
     }
 }
+
+/*void arrsum(int n, int arr[], int *sump) {
+    int i;
+    i = 0;
+    while (i < n) {
+        *sump = (*sump + arr[i]);
+        i = i + 1;
+    }
+}*/
