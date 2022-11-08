8b8fc8936e22a055e335b6802c35657ac5ea8429
Philip Kristian Møller Flyvholm <phif@itu.dk>
2022-11-08 11:32:17 +0100

    hell yeah

--- Ass(ignments)/Ass08/virtual/Selsort.java
+++ Ass(ignments)/Ass08/virtual/Selsort.java
@@ -20,10 +20,10 @@ class Selsort {
   public static void SelectionSort(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
       int least = i;                                      
-      for (int j = i+1; j < arr.length; j++)
-	if (arr[j] < arr[least])
-	  least = j;
-      int tmp = arr[i]; arr[i] = arr[least]; arr[least] = tmp;
+      for (int j = i+1; j < arr.length; j++) if (arr[j] < arr[least]) least = j;
+      int tmp = arr[i];
+      arr[i] = arr[least];
+      arr[least] = tmp;
     }
   }
 }
